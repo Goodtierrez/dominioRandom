8f88082a918d0500edc70527ec6077541c28a689
Jonathan Gutiérrez <jonathangutierrezperez@gmail.com>
2023-03-13 20:17:06 +0000

    nombre web

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+const dominioRandom = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let webName = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log("primer ciclo:", pronoun[i]);
+    for (let j = 0; j < adj.length; j++) {
+      console.log("segundo ciclo:", adj[j]);
+      for (let k = 0; k < noun.length; k++) {
+        console.log("tercer ciclo:", noun[k]);
+
+        webName.push(pronoun[i] + adj[j] + noun[k] + ".com");
+        console.log(webName);
+      }
+    }
+  }
+  return webName;
 };
+
+dominioRandom();
